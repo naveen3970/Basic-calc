be34437f8f44a073389d707cac06bb6fb59e83d4
naveen3970 <naveen.c@mitrahsoft.in>
2026-03-16 12:53:41 +0530

    first commit

---
+++ math.js
@@ -0,0 +1,15 @@
+function add(a,b){
+return a+b
+}
+function sub(a,b){
+    return a-b
+}
+function multiple(a,b){
+    return a*b
+}
+
+console.log(add(10,20));
+console.log(sub(30,15));
+console.log(multiple(2,3));
+
+
