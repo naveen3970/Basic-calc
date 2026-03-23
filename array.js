595613e396620bed5f36595da704d9ac919db1e0
dheena-ts <dheena.ts@mitrahsoft.in>
2026-03-23 13:23:24 +0530

    array.js is added to the basic-calc

---
+++ array.js
@@ -0,0 +1,12 @@
+const nums = [1, 2, 2, 3, 4, 4, 5];
+const uniqueNums = [...new Set(nums)];
+const max = Math.max(...nums);
+const min = Math.min(...nums);
+
+console.log(max);
+console.log(min);
+console.log(uniqueNums);
+
+
+
+
